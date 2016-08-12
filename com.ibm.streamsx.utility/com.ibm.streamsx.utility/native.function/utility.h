eed1d797bb36586090b23c73ba8082b68d4cad70
Tom Tran <tomtran@ca.ibm.com>
2016-08-12 14:33:26 -0500

    Added setThreadNodeAffinity for binding a tid to a NUMA node. Identical to setProcessNodeAffinity, which binds a pid,

--- com.ibm.streamsx.utility/com.ibm.streamsx.utility/native.function/utility.h
+++ com.ibm.streamsx.utility/com.ibm.streamsx.utility/native.function/utility.h
@@ -9,6 +9,7 @@
 namespace com { namespace ibm { namespace streamsx { namespace utility {
     uint64_t setCPUAffinity(uint64_t a); 
     uint64_t setProcessNodeAffinity(uint64_t a);
+    uint64_t setThreadNodeAffinity(uint64_t a);
     uint64_t getNodeCount(); 
     uint64_t addCPUAffinity(uint64_t a);
     void setCPUAffinity(std::vector<uint64_t> a);
